838ede2e725cf10453b06f27e53bb8e0994ab161
HyunsuYu <63137431+HyunsuYu@users.noreply.github.com>
2020-07-04 16:16:23 +0900

    Unity Runtime Data Path Management Update

--- UnityJsonDll/UnityJsonUtility.cs
+++ UnityJsonDll/UnityJsonUtility.cs
@@ -26,5 +26,108 @@ namespace UnityJsonDll
 
             return JsonUtility.FromJson<T>(jsonData);
         }
+
+        public static void writeStringToFile(string str, string filename)
+        {
+#if !WEB_BUILD
+            string path = pathForDocumentsFile(filename);
+            FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(file);
+            sw.WriteLine(str);
+
+            sw.Close();
+            file.Close();
+#endif 
+        }
+        public static string readStringFromFile(string filename)
+        {
+#if !WEB_BUILD
+            string path = pathForDocumentsFile(filename);
+
+            if (File.Exists(path))
+            {
+                FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(file);
+
+                string str = null;
+                str = sr.ReadLine();
+
+                sr.Close();
+                file.Close();
+
+                return str;
+            }
+            else
+            {
+                return null;
+            }
+#else
+        return null;
+#endif
+        }
+        public static string pathForDocumentsFile(string filename)
+        {
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                string path = Application.dataPath.Substring(0, Application.dataPath.Length - 5);
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(Path.Combine(path, "Documents"), filename);
+            }
+            else if (Application.platform == RuntimePlatform.Android)
+            {
+                string path = Application.persistentDataPath;
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(path, filename);
+            }
+            else
+            {
+                string path = Application.dataPath;
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(path, filename);
+            }
+        }
+        public static string pathForDocumentsFile(string filename, string folderpath)
+        {
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                string path = Application.dataPath.Substring(0, Application.dataPath.Length - 5);
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(Path.Combine(Path.Combine(path, "Documents"), folderpath), filename);
+            }
+            else if (Application.platform == RuntimePlatform.Android)
+            {
+                string path = Application.persistentDataPath;
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(Path.Combine(path, folderpath), filename);
+            }
+            else
+            {
+                string path = Application.dataPath;
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(Path.Combine(path, folderpath), filename);
+            }
+        }
+        public static string pathForDocumentsFolder(string folderPath)
+        {
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                string path = Application.dataPath.Substring(0, Application.dataPath.Length - 5);
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(Path.Combine(path, "Documents"), folderPath);
+            }
+            else if (Application.platform == RuntimePlatform.Android)
+            {
+                string path = Application.persistentDataPath;
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(path, folderPath);
+            }
+            else
+            {
+                string path = Application.dataPath;
+                path = path.Substring(0, path.LastIndexOf('/'));
+                return Path.Combine(path, folderPath);
+            }
+        }
     }
 }
